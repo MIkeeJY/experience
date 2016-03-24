0763ebcf9866153c0fa548db62f53b9e161d0f1a
张占永 <zzy0523@gmail.com>
2016-03-24 22:28:50 +0800

    删除多余的属性

--- data/src/main/java/cn/org/cfpamf/data/base/BaseViewHolder.java
+++ data/src/main/java/cn/org/cfpamf/data/base/BaseViewHolder.java
@@ -15,10 +15,7 @@ import android.view.View;
 
 public class BaseViewHolder extends RecyclerView.ViewHolder {
 
-    public View itemView;
-
     public BaseViewHolder(View itemView) {
         super(itemView);
-        this.itemView = itemView;
     }
 }
