67724fd0fef471b8caaf556f1fcb52cb56605acf
张占永 <zzy0523@gmail.com>
2016-03-29 22:46:23 +0800

    no message

---
+++ data/src/main/java/cn/org/cfpamf/data/okHttp/HttpConstant.java
@@ -0,0 +1,12 @@
+package cn.org.cfpamf.data.okHttp;
+
+/**
+ * Created by zzy on 16/3/29.
+ */
+public final class HttpConstant {
+    public static final String CONTENT_TYPE_KEY = "Content-Type";
+    public static final String CONTENT_TYPE = "application/json; charset=utf-8";
+    public static final String ACCEPT_KEY = "Accept";
+    public static final String ACCEPT = "application/json";
+    public static final String UTF8 = "utf8";
+}
